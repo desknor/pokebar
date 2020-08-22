0d6c061a3c35d81ccc23d188048611e36a465946
desknor <deschdev@gmail.com>
2020-08-22 18:00:25 -0400

    location choice form setup

--- src/components/LocationChoice.js
+++ src/components/LocationChoice.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { render } from 'react-dom';
 
 
 class LocationChoice extends React.Component {
   render() {
-    return <p>Cat in the hat</p>
+    return (
+      <Fragment>
+        <form className="location-choice">
+          <h2>Please Enter A Location</h2>
+          <input type="text" required placeholder="Location Choice"></input>
+          <button type ="submit">Visit Location -></button>
+        </form>
+      </Fragment>
+    )
   }
 }
 
