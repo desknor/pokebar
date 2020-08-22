d348896d06408a43a72c5aa7228e8551b0395e50
desknor <deschdev@gmail.com>
2020-08-22 17:26:31 -0400

    first component successful

---
+++ src/components/LocationChoice.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+
+class LocationChoice extends React.Component {
+  render() {
+    return <p>Cat in the hat</p>
+  }
+}
+
+export default LocationChoice;
