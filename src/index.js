e6952ebde68ab617557adfbff60c82b648ea327b
desknor <deschdev@gmail.com>
2020-08-22 20:33:06 -0400

    css added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import LocationChoice from './components/LocationChoice';
+import './css/style.css';
 
 
 render(<LocationChoice />, document.querySelector('#main'));
